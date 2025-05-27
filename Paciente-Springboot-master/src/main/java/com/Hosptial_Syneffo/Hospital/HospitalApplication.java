4990e68edfa6111a1ef2d0c81de2525df2dac8a4
Dangel <164272226+angelguillen20@users.noreply.github.com>
2025-05-26 22:37:55 -0400

    Add files via upload

---
+++ Paciente-Springboot-master/src/main/java/com/Hosptial_Syneffo/Hospital/HospitalApplication.java
@@ -0,0 +1,72 @@
+package com.Hosptial_Syneffo.Hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalApplication.class, args);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Paciente-Springboot-master/src/main/java/com/Hosptial_Syneffo/Hospital/model/Paciente.java
@@ -0,0 +1,45 @@
+package com.Hosptial_Syneffo.Hospital.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table (name="paciente")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPaciente; 
+
+    @Column(unique=true, length = 13, nullable=false)
+    private String runPaciente;
+
+    @Column(nullable=true)
+    private String nombrePaciente;
+
+    @Column(nullable=true)
+    private String genero;
+
+    @Column(nullable=true)
+    private String tipoSangre; 
+
+    @Column(nullable=true)
+    private Date fechaNacimiento; 
+
+    @Column(nullable=true)
+    private String estadoActual;
+
+
+}
